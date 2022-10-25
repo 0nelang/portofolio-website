e023c407f174075079a314d9e309dff9534079c5
0nelang <88448211+0nelang@users.noreply.github.com>
2022-10-25 14:08:27 +0700

    membenarkan user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'role' => 'admin',
+            'password' => '$2y$10$cJljZ8BeVZIEF6eVmePioODQtncD2FQwEJeKTFxDqgLRuqzr8qL12'
+        ]);
+        User::create([
+            'name' => 'walikelas',
+            'email' => 'walikelas@gmail.com',
+            'role' => 'walikelas',
+            'password' => '$2y$10$cJljZ8BeVZIEF6eVmePioODQtncD2FQwEJeKTFxDqgLRuqzr8qL12'
+        ]);
     }
 }
